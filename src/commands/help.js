6f733d2fc5b0de58d3d826fc51f8869a35dde7ad
Venikov Danil <melongames@mail.ru>
2021-06-15 12:38:37 +0200

    Localization fixed

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message) => {
         .setColor('#d76844')
         .setAuthor('Help section', 'https://assets.faceit-cdn.net/third_party/applications/cards/80a9a91e-7bd1-4200-adf8-fa13dc8a8e18-1623339995610.jpg')
         .setTitle('Commands:')
-        .setDescription('+ **fs!help** - open help section\n' + '+ **fs!player** \`[nickname]\` - show short player statistic\n' + '+ **fs!stats** \`[nickname]\` - show detailed player statistic')
+        .setDescription('+ **fs!help** - Open help section and displays all the bot commands\n' + '+ **fs!player** \`[nickname]\` - Display short player statistic\n' + '+ **fs!stats** \`[nickname]\` - Display detailed player statistic')
         .setFooter('This bot supports statistics from CS:GO only, but in the future statistics from other games and new features will be added :)')
     message.channel.send(helpMsgEmbed)
 }
